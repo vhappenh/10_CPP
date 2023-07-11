3f7c10145d3a5f6d9c02dffdce04f976a36304e2
Valerian Happenhofer <vhappenh@c4r4p7.42vienna.com>
2023-07-11 18:57:34 +0200

    pushy

--- 01_CPP/ex04/main.cpp
+++ 01_CPP/ex04/main.cpp
@@ -6,11 +6,13 @@
 /*   By: vhappenh <vhappenh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 18:36:32 by vhappenh          #+#    #+#             */
-/*   Updated: 2023/07/11 18:39:45 by vhappenh         ###   ########.fr       */
+/*   Updated: 2023/07/11 18:56:53 by vhappenh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main(int ac, char **av)
 {
@@ -19,9 +21,19 @@ int main(int ac, char **av)
         std::cout << "Wrong ammount of arguments" << std::endl;
         return (1);
     }
-    std::string filename = av[1];
+    const std::string filename = av[1];
     std::string str1 = av[2];
     std::string str2 = av[3];
-    std::cout << filename << std::endl;
-    std::cout << str1 << " " << str2 << std::endl;
+    std::string line;
+    std::ifstream readfile;
+    readfile.open(filename);
+    if (readfile.is_open())
+    {
+        while (std::getline (readfile, line))
+            std::cout << line << std::endl;
+        readfile.close();
+    }
+    else
+        std::cout << "Error when opening readfile" << std::endl;
+    //std::ofstream writefile;
 }
