27d3700ee66f0be59232c2c63bb4c3ba585ec2e8
Valerian Happenhofer <vhappenh@c4r7p1.42vienna.com>
2023-09-07 14:17:04 +0200

    megaphone checkup, removed one std::

--- 00_CPP/ex00/megaphone.cpp
+++ 00_CPP/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: vhappenh <vhappenh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 11:13:43 by vhappenh          #+#    #+#             */
-/*   Updated: 2023/06/21 12:50:41 by vhappenh         ###   ########.fr       */
+/*   Updated: 2023/09/07 11:00:02 by vhappenh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	main(int ac, char **av)
 			str += av[i];
 		std::string::iterator i;
 		for (i = str.begin(); i != str.end(); i++)
-			std::cout << (char)std::toupper(*i);	
+			std::cout << (char)toupper(*i);	
 	}
 	std::cout << std::endl;
 	return (0);
