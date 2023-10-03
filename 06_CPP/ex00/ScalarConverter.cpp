506485e1bce0c35ba91a80412280837bae0112f5
Valerian Happenhofer <vhappenh@c1r1p4.42vienna.com>
2023-10-03 16:38:28 +0200

    included nanf as valid input

--- 06_CPP/ex00/ScalarConverter.cpp
+++ 06_CPP/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: vhappenh <vhappenh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 11:49:00 by vhappenh          #+#    #+#             */
-/*   Updated: 2023/10/03 12:54:41 by vhappenh         ###   ########.fr       */
+/*   Updated: 2023/10/03 16:36:58 by vhappenh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ int	get_precision(std::string input) {
 bool	check_input(std::string input, double &n, double &pf, double &pd) {
 	char *endptr;
 	n = strtod(input.c_str(), &endptr);
-	if (endptr[0] && input.length() > 1 && input != "+inff" && input != "-inff" && input != "inff") {
+	if (endptr[0] && input.length() > 1 && input != "+inff" && input != "-inff" && input != "inff" && input != "nanf") {
 		std::cout << "Invalid input" << std::endl;
 		return (true);
 	}
