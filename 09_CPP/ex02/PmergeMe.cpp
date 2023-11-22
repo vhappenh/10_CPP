ea79e439b0d4f03eecad580e823cd7675f101f6a
Valerian Happenhofer <vhappenh@c2r6p5.42vienna.com>
2023-11-22 18:10:25 +0100

    back to base version, where I need a better vector structure to keep the pairs together

--- 09_CPP/ex02/PmergeMe.cpp
+++ 09_CPP/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: vhappenh <vhappenh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 14:23:59 by vhappenh          #+#    #+#             */
-/*   Updated: 2023/11/18 18:27:21 by vhappenh         ###   ########.fr       */
+/*   Updated: 2023/11/22 18:09:27 by vhappenh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,64 +37,43 @@ static long	jakobsthal(long n) {
 		return jakobsthal(n - 1) + 2 * jakobsthal(n - 2);
 }
 
-// static void	insert(std::vector<unsigned int>& main, std::vector<unsigned int> pend) {
-// 	main.insert(main.begin(), *pend.begin());
-// 	pend.erase(pend.begin());
-// 	while (pend.size()) {
-// 		unsigned int e = *(pend.end() - 1);
-// 		if (e < *(main.begin() + 1)) {
-// 			if (e < *main.begin()) {
-// 				main.insert(main.begin(), e);
-// 				pend.erase(pend.end() - 1);
-// 			}
-// 			else {
-// 				main.insert(main.begin() + 1, e);
-// 				pend.erase(pend.end() - 1);	
-// 			}
-// 		}
-// 		else {
-// 			if (e < *main.end()) {
-// 				main.insert(main.begin() + 1, e);
-// 				pend.erase(pend.end() - 1);	
-// 			}
-// 			else {
-// 				main.insert(main.end(), e);
-// 				pend.erase(pend.end() - 1);	
-// 			}
-// 		}	
-// 	}
-// 	print_vec(main, "insert main: ");
-// 	print_vec(pend, "insert pend: ");
-// 	std::cout << "\n";
-// }
+static unsigned int get_index( int breaky, int binaryinsertindex, unsigned int e, std::vector<unsigned int> main) {
+	if (e > main.at(binaryinsertindex)) {
+		if (breaky / 2)
+			binaryinsertindex = binaryinsertindex + breaky / 2;
+		else
+			binaryinsertindex = binaryinsertindex + 1;
+	}
+	else {
+		if (breaky / 2 || (binaryinsertindex > 0 && e > main.at(binaryinsertindex - 1)))
+			binaryinsertindex = binaryinsertindex - breaky / 2;
+		else
+			if (binaryinsertindex)
+				binaryinsertindex = binaryinsertindex - 1;
+	}
+	return (binaryinsertindex);
+}
 
-static void	insert_oag(std::vector<unsigned int>& main, std::vector<unsigned int> pend) {
-	main.insert(main.begin(), *pend.begin());
-	int	jakobs_mod = 3;
+static void	insert(std::vector<unsigned int>& main, std::vector<unsigned int> pend) {
+	int				jakobs_mod = 3;
 	unsigned int	count = pend.size() - 1;
+
+	main.insert(main.begin(), *pend.begin());
 	while (count) {
 		unsigned int jakob = jakobsthal(jakobs_mod) - 1;
-		// if (jakob > count) //use count instead
-		// 	jakob = count;
-		
-		// int blub = jakobsthal(jakobs_mod);
 		int i = jakob;
+		
+		if ((unsigned long)i >= pend.size())
+			i = pend.size() - 1;
+
 		while (i >= jakobsthal(jakobs_mod - 1)) {
 			unsigned int e = *(pend.begin() + i);
-			int 	binaryinsertindex = jakob / 2;
 			int		breaky = jakob;
- 			while (breaky) {
+			int 	binaryinsertindex = breaky / 2;
+			
+ 			while (breaky && (unsigned long)binaryinsertindex < main.size()) {
 				breaky /= 2;
-				if (e > main.at(binaryinsertindex)) {
-					binaryinsertindex = binaryinsertindex + breaky / 2;
-				}
-				else {
-					if (breaky / 2 || (binaryinsertindex > 0 && e > main.at(binaryinsertindex - 1)))
-						binaryinsertindex = binaryinsertindex - breaky / 2;
-					else
-						if (binaryinsertindex)
-							binaryinsertindex = binaryinsertindex - 1;
-				}
+				binaryinsertindex = get_index(breaky, binaryinsertindex, e, main);
 			}
 			main.insert(main.begin() + binaryinsertindex, e);
 			count--;
@@ -103,24 +82,14 @@ static void	insert_oag(std::vector<unsigned int>& main, std::vector<unsigned int
 		jakobs_mod++;
 	}
 	print_vec(main, "insert main: ");
-	print_vec(pend, "insert pend: ");
 	std::cout << "\n";
 }
 
 std::vector<unsigned int>	PmergeMe::solve(std::vector<unsigned int> pend) {
-	std::vector<unsigned int> main;
+	std::vector<unsigned int> 	main;				
 	
-	if (pend.size() < 2){
+	if (pend.size() < 2)
 		return (pend);
-	}
-	else if (pend.size() == 2) {
-		if (pend.at(0) > pend.at(1)){
-			unsigned int help = pend.at(0);
-			pend.at(0) = pend.at(1);
-			pend.at(1) = help;
-		}
-		return (pend);
-	}
 	for (std::vector<unsigned int>::iterator it = pend.begin(); it != pend.end(); it++) {
 		if ((it + 1) != pend.end() && *it > *(it + 1)) {
 			main.push_back(*it);
@@ -131,16 +100,9 @@ std::vector<unsigned int>	PmergeMe::solve(std::vector<unsigned int> pend) {
 			pend.erase((it + 1));
 		}
 	}
-	print_vec(main, "solve main: ");
-	print_vec(pend, "solve pend: ");
-	std::cout << "\n";
 	main = solve(main);
-	// if (pend.size() == 3)
-	// 	insert(main, pend);
-	// else
-		insert_oag(main, pend);
+	insert(main, pend);
 	return (main);
-	//print_vec(main, "last: ");
 }
 
 // void	PmergeMe::solve(std::list<unsigned int>& pend) {binaryinsertindex = binaryinsertindex + binaryinsertindex / 2;
